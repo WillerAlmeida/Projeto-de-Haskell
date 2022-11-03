a19682b6fb0bac89b4dccb1ba71fda57bbca1d15
WillerAlmeida <45805122+WillerAlmeida@users.noreply.github.com>
2022-11-02 22:10:02 -0300

    Update and rename haskell.hs to caixa1.hs

---
+++ caixa1.hs
@@ -0,0 +1,6 @@
+module Main (main) where -- Tarefa 1: Definição do módulo Main, exportando a variável main.
+import System.IO (stdout, hSetBuffering, BufferMode(NoBuffering))
+import System.Environment
+
+main :: IO() -- Tarefa 2: Anotar o tipo de main, que quando executada interage com o mundo e retorna a tupla vazia.
+
